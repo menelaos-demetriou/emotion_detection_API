c7fb0f379f9cb26f08a25fb3796b46ed2ec268cc
Menelaos Dimitriou <menelaos.dimitrio@gmail.com>
2020-08-25 14:03:20 +0000

    Created drag and drop page

--- app.py
+++ app.py
@@ -1,17 +1,28 @@
-from flask import Flask, request
+import os
+from flask import Flask, request, render_template
 from flask_dropzone import Dropzone
 
 app = Flask(__name__)
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app.config.update(
+    UPLOADED_PATH=os.path.join(basedir, 'uploads'),
+    # Flask-Dropzone config:
+    DROPZONE_ALLOWED_FILE_TYPE='image',
+    DROPZONE_MAX_FILE_SIZE=3,
+    DROPZONE_MAX_FILES=30,
+)
+
 dropzone = Dropzone(app)
 
-@app.route('/uploads', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def upload():
 
     if request.method == 'POST':
         f = request.files.get('file')
 
-    return "upload template"
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(host="178.128.40.146", debug=True)
